6b4b13b648cd06818b19929338935d74d904b5fb
KILLVader8550 <KILLVader8550@gmail.com>
2026-04-27 19:12:32 +0700

    server part

---
+++ server.c
@@ -0,0 +1,30 @@
+#include "minitalk.h"
+#include <signal.h>
+
+void  handler_signal(int signal)
+{
+    static unsigned char  character;
+    static int            bit;
+
+    if (bit < 8)
+    {
+        character |= (signal == SIGUSR1);
+        character <<= 1;
+        bit++;
+    }
+    else 
+    {
+        ft_fputchar(character);
+        bit = 0;
+        character = 0;
+    }
+}
+
+int main()
+{
+    signal(SIGUSR1, handler_signal);
+    signal(SIGUSR2, handler_signal);
+    while(1)
+        pause();
+    return (0);
+}
