337811315cdd71ca4fdfa96ee1574697c16c8611
KILLVader8550 <KILLVader8550@gmail.com>
2026-04-28 23:39:29 +0700

    bonus part

--- client.c
+++ client.c
@@ -1,5 +1,17 @@
 #include "minitalk.h"
 #include <stdio.h>
+#include <signal.h>
+
+int g_status = 0;
+
+static void ack(int signal)
+{
+    if (signal == SIGUSR2)
+        ft_fputstr("SEND SIGUSR2: SUCCESS\n");
+    else
+        ft_fputstr("SEND SIGUSR1: SUCCESS\n");
+    g_status = 1;
+}
 
 void  send_signal(pid_t server_pid, const unsigned char character)
 {
@@ -12,22 +24,22 @@ void  send_signal(pid_t server_pid, const unsigned char character)
    i = 0;
    while (i++ < 8)
    {
+       g_status = 0;
+
        if (buf & mask)
        {
-           printf("1 ");
            if (kill(server_pid, SIGUSR1) == -1)
                ft_fputstr("Errow while sending\n");
        }
        else
        {
-           printf("0 ");
            if (kill(server_pid, SIGUSR2) == -1)
                ft_fputstr("Error while sending\n");
        }
        mask >>= 1;
-       usleep(42);
+       while (g_status != 1)
+           pause();
    }
-   printf("\n");
 }
 
 int main(int argc, char **argv)
@@ -42,6 +54,8 @@ int main(int argc, char **argv)
     }
     i = 0;
     server_pid = ft_atoi(argv[1]);
+    signal(SIGUSR1, ack);
+    signal(SIGUSR2, ack);
     while (argv[2][i])
         send_signal(server_pid, argv[2][i++]);
     send_signal(server_pid, '\0');
--- server.c
+++ server.c
@@ -1,11 +1,23 @@
 #include "minitalk.h"
 #include <stdio.h>
 
-void  handler_signal(int signal)
+static void	*ft_memset(void *s, int c, size_t n)
+{
+	const unsigned char	chr = c;
+	unsigned char		*p;
+
+	p = s;
+	while (n-- > 0)
+		*p++ = chr;
+	return (s);
+}
+
+void  handler_signal(int signal, siginfo_t *info, void *context)
 {
     static unsigned char  character = 0;
     static int            bit = 0;
 
+    (void)context;
     bit++;
     character |= (signal == SIGUSR1);
     if (bit == 8)
@@ -19,15 +31,23 @@ void  handler_signal(int signal)
     }
     else
         character <<= 1;
+    if (kill(info->si_pid, signal) == -1)
+        ft_fputstr("Can't acknowledge\n");
 }
 
 int main()
 {
+    struct sigaction sa;
+
+    ft_memset(&sa, 0, sizeof(sa));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = &handler_signal;
+    sa.sa_flags = SA_SIGINFO;
     ft_fputstr("PID: ");
     ft_fputnbr_deci(getpid());
     ft_fputchar('\n');
-    signal(SIGUSR1, handler_signal);
-    signal(SIGUSR2, handler_signal);
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
     while(1)
         pause();
     return (0);
